36bf0d479fdf45f3a686d3f6333226c3ff899c3c
Nutz3r <antoinelespagnard@gmail.com>
2023-06-09 12:10:06 +0200

    et la base de données bien sûr

--- bdd/portfolio.sql
+++ bdd/portfolio.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : mar. 06 juin 2023 à 13:57
--- Version du serveur : 5.7.36
--- Version de PHP : 7.4.26
+-- Généré le : ven. 09 juin 2023 à 10:09
+-- Version du serveur : 8.0.31
+-- Version de PHP : 8.0.26
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -29,10 +29,10 @@ SET time_zone = "+00:00";
 
 DROP TABLE IF EXISTS `categories`;
 CREATE TABLE IF NOT EXISTS `categories` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int NOT NULL AUTO_INCREMENT,
   `nom` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=29 DEFAULT CHARSET=utf8mb4;
+) ENGINE=MyISAM AUTO_INCREMENT=31 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Déchargement des données de la table `categories`
@@ -42,10 +42,12 @@ INSERT INTO `categories` (`id`, `nom`) VALUES
 (0, 'Non classé'),
 (28, 'Photographie'),
 (27, 'InDesign'),
-(26, 'Vidéo'),
+(26, 'PremierPro'),
 (25, 'Web'),
 (23, 'Illustrator'),
-(24, 'Photoshop');
+(24, 'Photoshop'),
+(29, 'Blender'),
+(30, 'Figma');
 
 -- --------------------------------------------------------
 
@@ -55,13 +57,13 @@ INSERT INTO `categories` (`id`, `nom`) VALUES
 
 DROP TABLE IF EXISTS `contact`;
 CREATE TABLE IF NOT EXISTS `contact` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int NOT NULL AUTO_INCREMENT,
   `nom` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `message` text NOT NULL,
   `date` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
+) ENGINE=MyISAM AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -71,20 +73,26 @@ CREATE TABLE IF NOT EXISTS `contact` (
 
 DROP TABLE IF EXISTS `galerie`;
 CREATE TABLE IF NOT EXISTS `galerie` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `id_categorie` int(11) NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
+  `frontPage` tinyint(1) NOT NULL,
+  `id_categorie` int NOT NULL,
   `nom` varchar(255) NOT NULL,
   `description` text NOT NULL,
   `image` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=23 DEFAULT CHARSET=utf8mb3;
 
 --
 -- Déchargement des données de la table `galerie`
 --
 
-INSERT INTO `galerie` (`id`, `id_categorie`, `nom`, `description`, `image`) VALUES
-(13, 26, 'Illustrator', 'oiuyt', '1118864491338016793video-1.jpg');
+INSERT INTO `galerie` (`id`, `frontPage`, `id_categorie`, `nom`, `description`, `image`) VALUES
+(17, 0, 23, 'Maman et moi Pâtisserie', 'Menu d\'Halloween pour la pâtisserie &quot;Maman et moi&quot; ', '1597842295Maman-et-moi-Py-tisserie-Affiche-Halloween-2.png'),
+(18, 0, 23, 'Ombre inventée', 'Travail réalisé au cours de Photoshop', '2022765614T6-Ombres-inventy-es-Antoine-Lespagnard.jpg'),
+(19, 0, 23, 'Morphing', 'Travail réalisé au cours de Photoshop', '1386273704T8-Morphing-Antoine-Lespagnard.jpg'),
+(20, 0, 23, 'Nike Air Max', 'Maquette réalisée au cours de Web Design/UX', '224695869NikeAirMax.png'),
+(21, 0, 23, 'e', 'zae', '1566612189NikeAirMax.png'),
+(22, 1, 23, 'eazeaze', 'azea', '708451558NikeAirMax.png');
 
 -- --------------------------------------------------------
 
@@ -94,12 +102,12 @@ INSERT INTO `galerie` (`id`, `id_categorie`, `nom`, `description`, `image`) VALU
 
 DROP TABLE IF EXISTS `identifiants`;
 CREATE TABLE IF NOT EXISTS `identifiants` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int NOT NULL AUTO_INCREMENT,
   `login` varchar(60) NOT NULL,
   `password` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb3;
 
 --
 -- Déchargement des données de la table `identifiants`
@@ -116,22 +124,27 @@ INSERT INTO `identifiants` (`id`, `login`, `password`, `email`) VALUES
 
 DROP TABLE IF EXISTS `skills`;
 CREATE TABLE IF NOT EXISTS `skills` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int NOT NULL AUTO_INCREMENT,
   `nom` varchar(70) NOT NULL,
   `fichier` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4;
+) ENGINE=MyISAM AUTO_INCREMENT=23 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Déchargement des données de la table `skills`
 --
 
 INSERT INTO `skills` (`id`, `nom`, `fichier`) VALUES
-(6, 'Premiere Pro', ''),
-(14, 'Illustrator', '338016793video-1.jpg'),
-(15, 'After Effect', ''),
-(16, 'Figma', ''),
-(17, 'Photogrpahy', '');
+(6, 'Premiere Pro', '7220177111640093594Premiere-Pro.png'),
+(14, 'Illustrator', '437414111068335931illustrator.png'),
+(15, 'After Effect', '1602784272112576778aftereffect.png'),
+(16, 'Figma', '1239163389pngegg.png'),
+(17, 'Photography', '1989864587photographyicon.png'),
+(18, 'InDesign', '1607392536Adobe-InDesign-CC-icon.svg.png'),
+(19, 'PHP', '121772851pngegg-5-.png'),
+(20, 'HTML5', '1797789049pngegg-3-.png'),
+(21, 'CSS', '355736861pngegg-4-.png'),
+(22, 'Blender', '502058241pngegg-1-.png');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
